837cbf0a92dd093ef7065e7f944604348156b8d9
wtbno <bruno_links@hotmail.com>
2022-01-24 20:41:28 -0300

    dotnet init

---
+++ myApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Tipos primitivos - Values Types
+//Numéricos
+/*
+int
+decimal
+double
+Outros
+Boolean
+Char
+Tuples
+
+->Reference types
+classe
+interface
+delegate
+record
+object
+string(não possui limite de tamanho)
+*/
